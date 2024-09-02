d50a5737ef9a52ed41ea70fdc6e58d68ae3829f4
Gooyoo2022 <110799533+Gooyoo2022@users.noreply.github.com>
2024-09-02 22:39:36 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -894,7 +894,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://rci.ccitel.net:8091".to_owned()
+    "https://rci.ccitel.net:8083".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
